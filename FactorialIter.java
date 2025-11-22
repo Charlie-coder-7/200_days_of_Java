6252952ffe9d449dc6b0bee6b0ae34906f0c4dbb
Charlie-coder-7 <sahooachyuta168@gmail.com>
2025-11-22 21:19:48 +0530

    Add files via upload

---
+++ FactorialIter.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class FactorialIter {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        BigInteger fact = BigInteger.ONE; // use BigInteger to avoid overflow
+        for (int i = 2; i <= n; i++) fact = fact.multiply(BigInteger.valueOf(i));
+        System.out.println(fact);
+        sc.close();
+    }
+}

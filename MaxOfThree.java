e05d5637a55f84b03cf38d9885c34411ccf11226
Charlie-coder-7 <sahooachyuta168@gmail.com>
2025-12-01 23:41:03 +0530

    Add files via upload

---
+++ MaxOfThree.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class MaxOfThree {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int max = Math.max(a, Math.max(b, c));
+        System.out.println(max);
+        sc.close();
+    }
+}

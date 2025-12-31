4b7b064dd23c87334b87c557423d32806ab3e661
Charlie-coder-7 <sahooachyuta168@gmail.com>
2025-12-31 21:16:47 +0530

    Add files via upload

---
+++ HourglassPattern.java
@@ -0,0 +1,17 @@
+public class HourglassPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = n - i; j > 0; j--) System.out.print(" ");
+            for (int j = 1; j <= 2*i - 1; j++) System.out.print("*");
+            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = n - i; j > 0; j--) System.out.print(" ");
+            for (int j = 1; j <= 2*i - 1; j++) System.out.print("*");
+            System.out.println();
+        }
+    }
+}

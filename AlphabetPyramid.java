890546ec1b93dda0feb591d433a55d6298078ce7
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-01-04 21:28:44 +0530

    Add files via upload

---
+++ AlphabetPyramid.java
@@ -0,0 +1,10 @@
+public class AlphabetPyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            char ch = 'A';
+            for (int j = 0; j <= i; j++) System.out.print(ch++ + " ");
+            System.out.println();
+        }
+    }
+}

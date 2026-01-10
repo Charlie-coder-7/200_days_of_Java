5afd65054239e0013f18e1bbb60773fb0a767d70
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-01-10 23:55:00 +0530

    Add files via upload

---
+++ MaxElement.java
@@ -0,0 +1,9 @@
+
+public class MaxElement {
+    public static void main(String[] args) {
+        int[] arr = {3,5,1,9,2};
+        int max = arr[0];
+        for(int x : arr) if(x > max) max = x;
+        System.out.println(max);
+    }
+}

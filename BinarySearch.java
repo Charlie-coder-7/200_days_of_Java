accaeccec724d1a95ae02a19bf7c9df0b4f60307
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-01-16 19:16:35 +0530

    Add files via upload

---
+++ BinarySearch.java
@@ -0,0 +1,17 @@
+
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,7,9};
+        int key = 7, l = 0, r = arr.length - 1;
+
+        while(l <= r){
+            int mid = (l + r) / 2;
+            if(arr[mid] == key){
+                System.out.println("Found");
+                return;
+            } else if(arr[mid] < key) l = mid + 1;
+            else r = mid - 1;
+        }
+        System.out.println("Not found");
+    }
+}

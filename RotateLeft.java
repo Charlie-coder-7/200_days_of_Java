306fd8d819d83122ed8ab7fb16f386cfc0fac3af
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-01-18 22:45:02 +0530

    Add files via upload

---
+++ RotateLeft.java
@@ -0,0 +1,12 @@
+public class RotateLeft {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
+        int k = 1;
+        for(int i=0;i<k;i++){
+            int first = arr[0];
+            for(int j=0;j<arr.length-1;j++) arr[j]=arr[j+1];
+            arr[arr.length-1] = first;
+        }
+        for(int x : arr) System.out.print(x+" ");
+    }
+}

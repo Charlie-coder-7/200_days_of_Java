7b3ecb1b3c69482479ac0a83175485ee4142bba4
ACHYUTA SAHOO <sahooachyuta168@gmail.com>
2026-01-25 21:24:59 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,8 @@
+public class SumArray {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int sum = 0;
+        for(int x : arr) sum += x;
+        System.out.println(sum);
+    }
+}
